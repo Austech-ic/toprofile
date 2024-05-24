beda2b6749c79be05555ddf0d089a4be48f1726f
DevMayani <106169881+DevMayani@users.noreply.github.com>
2024-05-24 11:17:37 +0100

    add

--- landing-page/src/app/layout.js
+++ landing-page/src/app/layout.js
@@ -1,7 +1,7 @@
 import Footer from "@/components/websites/Footer/footer";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        
+      <Head>
+        <link rel="icon" href="/brand/to.jpeg" />
+        <link rel="icon" sizes="192x192" href="/brand/to.jpeg" />
+        <link rel="icon" type="image/png" href="/brand/to.jpeg" />
+        <link rel="icon" sizes="128x128" href="/brand/to.jpeg" />
+      </Head>
         {children}
         <Footer />
         </body>
